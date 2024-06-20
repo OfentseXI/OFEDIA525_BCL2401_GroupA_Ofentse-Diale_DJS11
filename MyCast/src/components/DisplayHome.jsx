a5f88592d333136fba7e9eacd23e4d89bb375f57
OfentseXI <dofentse69@gmail.com>
2024-06-20 14:16:24 +0200

    Updated DisplayHome component to render AlbumItem components and added Todays Biggest Hits section; added CSS rule to hide webkit scrollbar

--- MyCast/src/components/DisplayHome.jsx
+++ MyCast/src/components/DisplayHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
 
 const DisplayHome = () => {
   return (
@@ -10,9 +11,14 @@ const DisplayHome = () => {
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Featured Casts</h1>
         <div className='flex overflow-auto'>
-
+            {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
         </div>
-        {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+      </div>
+      <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Todays Biggest Hits</h1>
+        <div className='flex overflow-auto'>
+            {}
+        </div>
       </div>
     </>
   )
