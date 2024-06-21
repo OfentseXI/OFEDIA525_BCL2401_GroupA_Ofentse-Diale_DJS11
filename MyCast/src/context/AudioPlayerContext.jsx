5bb802c974a05c55d53bba85d759fc09c67c7194
OfentseXI <dofentse69@gmail.com>
2024-06-21 09:06:18 +0200

    Added AudioPlayerContext with refs, state, and functions; updated AudioPlayer component to use context

--- MyCast/src/context/AudioPlayerContext.jsx
+++ MyCast/src/context/AudioPlayerContext.jsx
@@ -1,13 +1,45 @@
-import { createContext, useRef } from "react";
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
 
 export const AudioPlayerContext = createContext();
 
 const AudioPlayerContextProvider = (props) => {
 
     const audioRef = useRef();
+    const seekBg = useRef();
+    const seekBar = useRef();
+
+    const [track,setTrack] = useState(songsData[0]);
+    const [playing,setPlaying] = useState(false);
+    const [time,setTime] = useState({
+        currentTime:{
+            second: 0,
+            minute: 0
+        },
+        totalTime: {
+            second: 0,
+            minute: 0
+        }
+    });
+
+    const play = () => {
+        audioRef.current.play();
+        setPlaying(true);
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlaying(false);
+    }
 
     const contextValue = {
-        audioRef
+        audioRef,
+        seekBar,
+        seekBg,
+        track,setTrack,
+        playing,setPlaying,
+        time,setTime, 
+        play,pause
     }
 
     return (
